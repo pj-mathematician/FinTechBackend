1df8fb7e1c8e3ddb2622aed880e4f39fc35463a9
Poojan Vachharajani <pjmathematician@gmail.com>
2022-03-07 18:09:00 +0530

    Login Register API implemented with CORS

--- Back/settings.py
+++ Back/settings.py
@@ -150,6 +150,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
